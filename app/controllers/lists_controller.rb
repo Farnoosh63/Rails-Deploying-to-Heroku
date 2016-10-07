00f1ee82b6f00f9a624aa95634e598f3d7531be5
Farnoosh <farnoosh@pdx.edu>
2016-10-07 12:57:47 -0700

    add destroy task.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -45,6 +45,6 @@ class ListsController < ApplicationController
 
   private
   def list_params
-    params.require(:list).permit(:name, :description)
+    params.require(:list).permit(:name)
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,27 @@ class TasksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
     @task = @list.tasks.new
+    render :new
   end
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(task_params)
+    if @task.save
+      redirect_to lists_path(@task.list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id]).destroy
+    redirect_to list_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:description)
+  end
+
 end
